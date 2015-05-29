94c60927cfbed30516f3d6351adbb7f7dd8d9493
woniu17 <qinglucklin@foxmail.com>
2015-05-29 20:49:00 +0800

    mod pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,12 +3,15 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'qingluck'
+AUTHOR_EMAIL = u'qinglucklin@foxmail.com'
 SITENAME = u'qingtech'
-SITEURL = ''
+SITEURL = 'http://woniu17.github.io'
+DEFAULT_DATE_FORMATE = ('%Y-%m-%d')
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+#TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = u'zh'
 
@@ -20,15 +23,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+         ('LIN Qingxiang', 'http://www.linqingxiang.com/'),
+        )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+          ('github', 'https://github.com/woniu17'),
+         )
 
+THEME = 'pelican-themes/simple-bootstrap'
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
